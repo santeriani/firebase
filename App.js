8c57d71cbe16297cf1e6ab8e52d38ac8be7165a3
Santeri Anias <santeri.anias@gmail.com>
2024-03-09 23:47:19 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, SafeAreaView, ScrollView } from 'react-native';
+import { firestore,collection,addDoc,serverTimestamp,MESSAGES } from './firebase/Config';
+import { useEffect, useState } from 'react';
+import { QuerySnapshot, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { convertFirebaseTimeStampToJS } from './helpers/Function';
 
 export default function App() {
+  const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+
+  const save = async() => {
+    const docRef = await addDoc(collection(firestore, MESSAGES), {
+      text: newMessage,
+      created: serverTimestamp()
+    }).catch (error => console.log(error))
+
+    setNewMessage('')
+    console.log('Message saved.')
+  }
+
+  useEffect(() => {
+    const q = query(collection(firestore,MESSAGES),orderBy('created','desc'))
+    
+    const unsubscribe = onSnapshot(q,(querySnapshot) => {
+      const tempMessages =[]
+
+      querySnapshot.forEach((doc) => {
+        const messageObject = {
+          id: doc.id,
+          text: doc.date().text,
+          created: convertFirebaseTimeStampToJS(doc.data().created)
+        }
+
+        tempMessages.push(messageObject)
+      })
+      setMessages(tempMessages)
+    })
+    return() => {
+      unsubscribe()
+    }
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {
+          messages.map((message) =>(
+            <View style={styles.message} key={message.id}>
+              <Text style={styles.messageInfo}>{message.created}</Text>
+              <Text>{message.text}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  message: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#f5f5f5',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  messageInfo: {
+    fontSize: 12
+  }
 });
---
+++ firebase/Config.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore,collection,addDoc,serverTimestamp } from 'firebase/firestore';
+  
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+initializeApp(firebaseConfig);
+
+const firestore = getFirestore();
+
+const MESSAGES = 'messages';
+
+export {
+    firestore,
+    collection,
+    addDoc,
+    serverTimestamp,
+    MESSAGES
+};
